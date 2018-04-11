0ed97d692a50a0fb5dffcbeda7fc8d6d763a14d5
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-11 12:17:31 +0900

    clousure

--- Digital Timer01/ViewController.swift
+++ Digital Timer01/ViewController.swift
@@ -9,15 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timelbl: UILabel!
+    @IBOutlet weak var timeswitch: UISwitch!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+        //클로저(Closure)의 사용
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (myTimer) in
+            self.updateTime()
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE"
+        timelbl.text = formatter.string(from: date)
     }
 
 
